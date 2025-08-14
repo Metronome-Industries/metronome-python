9cfeb7930c68553fe3b8d70c0edd92084e5ee767
Tyler Gibbons <tyler@metronome.com>
2025-08-14 14:09:08 -0700

    fix: transform iso8601 timestamps to be rfc3339-compliant

--- src/metronome/_utils/_transform.py
+++ src/metronome/_utils/_transform.py
@@ -230,7 +230,10 @@ def _transform_recursive(
 def _format_data(data: object, format_: PropertyFormat, format_template: str | None) -> object:
     if isinstance(data, (date, datetime)):
         if format_ == "iso8601":
-            return data.isoformat()
+            if data.microsecond == 0:
+                return data.isoformat(sep="T", timespec="seconds").replace("+00:00", "Z")
+            else:
+                return data.isoformat(sep="T", timespec="milliseconds").replace("+00:00", "Z")
 
         if format_ == "custom" and format_template is not None:
             return data.strftime(format_template)
